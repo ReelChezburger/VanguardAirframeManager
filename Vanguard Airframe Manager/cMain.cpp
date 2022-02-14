054f90a7a29909d8bd5837b31e23689c73e07706
ReelChezburger <58400402+ReelChezburger@users.noreply.github.com>
2022-02-14 16:53:15 -0600

    Added browse function

--- Vanguard Airframe Manager/cMain.cpp
+++ Vanguard Airframe Manager/cMain.cpp
@@ -1,18 +1,22 @@
 #include "cMain.h"
 
+wxBEGIN_EVENT_TABLE(cMain, wxFrame)
+	EVT_BUTTON(10001, cMain::browseForXPlane)
+wxEND_EVENT_TABLE()
+
 cMain::cMain() : wxFrame(nullptr, wxID_ANY, "Vanguard Airframe Manager")
 {
 	this->SetBackgroundColour(wxColor(169, 169, 169));
 
-	// browse panel defenitions
+	// Browse panel defenitions
 	browsePanel = new wxPanel(this, wxID_ANY, wxDefaultPosition, wxSize(600, 100));
 	browsePanel->SetBackgroundColour(wxColor(255, 255, 255));
 
 	browseTitle = new wxStaticText(browsePanel, wxID_ANY, "X-Plane Location", wxDefaultPosition, wxSize(580,20), wxALIGN_CENTRE_HORIZONTAL);
-	xPlaneLocationBar = new wxTextCtrl(browsePanel, wxID_ANY, "Set X-Plane Location Here", wxDefaultPosition, wxSize(400, 50));
-	browseButton = new wxButton(browsePanel, wxID_ANY, "Browse", wxDefaultPosition, wxSize(100, 50));
+	xPlaneLocationBar = new wxTextCtrl(browsePanel, wxID_ANY, "Set X-Plane Location With Browse", wxDefaultPosition, wxSize(400, 50), wxTE_READONLY | wxTE_CENTRE | wxTE_BESTWRAP);
+	browseButton = new wxButton(browsePanel, 10001, "Browse", wxDefaultPosition, wxSize(100, 50));
 
-	// airframe panel defenitions
+	// Airframe panel defenitions
 	airframePanel = new wxPanel(this, wxID_ANY, wxDefaultPosition, wxSize(295, 300));
 	airframePanel->SetBackgroundColour(wxColor(255, 255, 255));
 
@@ -21,7 +25,7 @@ cMain::cMain() : wxFrame(nullptr, wxID_ANY, "Vanguard Airframe Manager")
 	airframeListRefresh = new wxButton(airframePanel, wxID_ANY, "Refresh", wxDefaultPosition, wxSize(132, 50));
 	airframeOpen = new wxButton(airframePanel, wxID_ANY, "Open", wxDefaultPosition, wxSize(132, 50));
 
-	// import/export panel defenitions
+	// Import/Export panel defenitions
 	importExportPanel = new wxPanel(this, wxID_ANY, wxDefaultPosition, wxSize(295, 300));
 	importExportPanel->SetBackgroundColour(wxColor(255, 255, 255));
 
@@ -34,7 +38,7 @@ cMain::cMain() : wxFrame(nullptr, wxID_ANY, "Vanguard Airframe Manager")
 	importPanel->SetBackgroundColour(wxColor(169, 169, 169));
 	importButton = new wxButton(importPanel, wxID_ANY, "Import from ZIP", wxDefaultPosition, wxSize(255, 70));
 
-	// browse panel sizer
+	// Browse panel sizer
 	wxBoxSizer* verticalSizer = new wxBoxSizer(wxVERTICAL);
 	verticalSizer->Add(browsePanel, 0, wxEXPAND | wxALL, 10);
 
@@ -48,12 +52,12 @@ cMain::cMain() : wxFrame(nullptr, wxID_ANY, "Vanguard Airframe Manager")
 	browseVerticalSizer->Add(browseHorizontalSizer, 5, wxEXPAND | wxBOTTOM | wxLEFT | wxRIGHT, 10);
 	browsePanel->SetSizerAndFit(browseVerticalSizer);
 
-	// bottom panels sizer
+	// Bottom panels sizer
 	wxBoxSizer* horizontalSizer = new wxBoxSizer(wxHORIZONTAL);
 	horizontalSizer->Add(airframePanel, 1, wxEXPAND | wxRIGHT, 10);
 	horizontalSizer->Add(importExportPanel, 1, wxEXPAND | wxALL, 0);
 
-	//airframe panel sizer
+	// Airframe panel sizer
 	wxBoxSizer* airframeVerticalSizer = new wxBoxSizer(wxVERTICAL);
 	airframeVerticalSizer->Add(airframeTitle, 0, wxEXPAND | wxALL, 10);
 	airframeVerticalSizer->Add(airframeList, 1, wxEXPAND | wxLEFT | wxRIGHT | wxBOTTOM, 10);
@@ -65,7 +69,7 @@ cMain::cMain() : wxFrame(nullptr, wxID_ANY, "Vanguard Airframe Manager")
 	airframeVerticalSizer->Add(airframeHorizontalSizer, 0, wxEXPAND | wxBOTTOM | wxLEFT | wxRIGHT, 10);
 	airframePanel->SetSizerAndFit(airframeVerticalSizer);
 
-	// import/export panel sizer
+	// Import/export panel sizer
 	wxBoxSizer* importExportSizer = new wxBoxSizer(wxVERTICAL);
 	importExportSizer->Add(exportPanel, 2, wxEXPAND | wxALL, 10);
 	importExportSizer->Add(importPanel, 1, wxEXPAND | wxRIGHT | wxLEFT | wxBOTTOM, 10);
@@ -80,7 +84,7 @@ cMain::cMain() : wxFrame(nullptr, wxID_ANY, "Vanguard Airframe Manager")
 	exportPanel->SetSizerAndFit(exportSizer);
 	importPanel->SetSizerAndFit(importSizer);
 
-	// configures main sizers relative to each-other
+	// Configures main sizers relative to each-other
 	importExportPanel->SetSizerAndFit(importExportSizer);
 
 	verticalSizer->Add(horizontalSizer, 3, wxEXPAND | wxBOTTOM | wxLEFT | wxRIGHT, 10);
@@ -96,12 +100,45 @@ void cMain::firstBoot()
 {
 	wxMessageBox(wxT("It appears that this is the first time you are running this program.\nPlease set your X-Plane location under the \"X-Plane Location\" window."),
 		wxT("First Boot"), wxOK | wxCENTRE | wxSTAY_ON_TOP, this);
+	return;
 }
 
 void cMain::pathNotSet()
 {
 	wxMessageBox(wxT("Please set your X-Plane location under the \"X-Plane Location\" window."),
 		wxT("Path Not Set"), wxOK | wxCENTRE | wxSTAY_ON_TOP, this);
+	return;
+}
+
+void cMain::pathNotFound()
+{
+	wxMessageBox(wxT("Your X-Plane location does not appear to be valid.\nPlease check your X-Plane location under the \"X-Plane Location\" window."),
+		wxT("Path Not Valid"), wxOK | wxCENTRE | wxSTAY_ON_TOP | wxICON_ERROR, this);
+	return;
+}
+
+void cMain::browseForXPlane(wxCommandEvent& WXUNUSED(event))
+{
+	wxDirDialog
+		openDirDialog(this, _("Open your X-Plane 11 folder"), "", wxDD_DIR_MUST_EXIST);
+	if (openDirDialog.ShowModal() == wxID_CANCEL)
+		return;     // the user changed idea...
+
+	// proceed loading the file chosen by the user;
+	// this can be done with e.g. wxWidgets input streams:
+	if (!wxDirExists(openDirDialog.GetPath()))
+	{
+		pathNotFound();
+		return;
+	}
+	else
+	{
+		xPlanePath.Add(openDirDialog.GetPath());
+		xPlanePath.Add(openDirDialog.GetPath() + wxFileName::GetPathSeparator() + "Output" + wxFileName::GetPathSeparator() + "CL650" + wxFileName::GetPathSeparator() + "airframes");
+		xPlaneLocationBar->SetLabel(xPlanePath[0]);
+		writeConfig("path", xPlanePath[0]);
+	}
+	return;
 }
 
 void cMain::readConfig()
@@ -114,10 +151,20 @@ void cMain::readConfig()
 	wxFileConfig* ConfigINI = new wxFileConfig("Vanguard Airframe Manager", wxEmptyString, configPath);
 	if (ConfigINI->wxConfigBase::Read("path", &pathValue))
 	{
-		if (pathValue == "0") {
+		if (pathValue == "0") 
+		{
 			pathNotSet();
 		}
-		// add an else if to check if the path is valid
+		else if (!wxDirExists(pathValue)) 
+		{
+			pathNotFound();
+		}
+		else 
+		{
+			xPlanePath.Add(pathValue);
+			xPlanePath.Add(pathValue + wxFileName::GetPathSeparator() + "Output" + wxFileName::GetPathSeparator() + "CL650" + wxFileName::GetPathSeparator() + "airframes");
+			xPlaneLocationBar->SetLabel(xPlanePath[0]);
+		}
 		delete ConfigINI;
 	} else
 	{
@@ -126,4 +173,14 @@ void cMain::readConfig()
 		delete ConfigINI;
 		firstBoot();
 	}
+	return;
+}
+
+void cMain::writeConfig(wxString writeKey, wxString writeValue)
+{
+	wxFileConfig* ConfigINI = new wxFileConfig("Vanguard Airframe Manager", wxEmptyString, configPath);
+	ConfigINI->Write(writeKey, writeValue);
+	ConfigINI->Flush();
+	delete ConfigINI;
+	return;
 }
--- Vanguard Airframe Manager/cMain.h
+++ Vanguard Airframe Manager/cMain.h
@@ -6,6 +6,7 @@
 #include <wx/fileconf.h>
 #include <wx/file.h>
 #include <wx/dir.h>
+#include <wx/filedlg.h>
 
 class cMain : public wxFrame
 {
@@ -14,41 +15,48 @@ public:
 	~cMain();
 
 public:
-	// browse panel
+	// Browse panel
 	wxPanel* browsePanel = nullptr;
 	wxStaticText* browseTitle = nullptr;
 	wxTextCtrl* xPlaneLocationBar = nullptr;
 	wxButton* browseButton = nullptr;
 
-	// airframe list
+	// Airframe list
 	wxPanel* airframePanel = nullptr;
 	wxStaticText* airframeTitle = nullptr;
 	wxListBox* airframeList = nullptr;
 	wxButton* airframeListRefresh = nullptr;
 	wxButton* airframeOpen = nullptr;
 
-	// import/export
+	// Import/Export
 	wxPanel* importExportPanel = nullptr;
 
-	//export
+	// Export
 	wxPanel* exportPanel = nullptr;
 	wxStaticText* exportTitle = nullptr;
 	wxTextCtrl* exportBox = nullptr;
 	wxButton* exportButton = nullptr;
 
-	//import
+	// Import
 	wxPanel* importPanel = nullptr;
 	wxStaticText* importTitle = nullptr;
 	wxButton* importButton = nullptr;
 
 	// Configuration
+	wxPathList xPlanePath;
+
 	wxString configDir = wxStandardPaths::Get().GetUserConfigDir() + wxFileName::GetPathSeparator() + "Vanguard Airframe Manager";
 	wxString configPath = configDir + wxFileName::GetPathSeparator() + "VAMConfig.ini";
 
+	void browseForXPlane(wxCommandEvent& event);
+
 	void firstBoot();
+	void pathNotSet();
+	void pathNotFound();
 
 	void readConfig();
-	void pathNotSet();
-	wxPathList xPlanePath;
-};
+	void writeConfig(wxString writeKey, wxString writeValue);
 
+private:
+	wxDECLARE_EVENT_TABLE();
+};
